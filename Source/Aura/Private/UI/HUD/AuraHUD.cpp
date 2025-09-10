45a5cce4d2c8bdfa7f929a82937a56e70ace6829
0xJarry <okanella@gmail.com>
2025-09-10 13:17:25 -0500

    33 Overlay Widget Controller, what a doozy

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+
+UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetControllerParams& WcParams)
+{
+	if (OverlayWidgetController == nullptr)
+	{
+		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
+		OverlayWidgetController->SetWidgetControllerParams(WcParams);
+		
+		return OverlayWidgetController;
+	}
+	return OverlayWidgetController;
+}
+
+void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC,
+	UAttributeSet* AttributeSet)
+{
+	checkf(OverlayWidgetClass, TEXT("Overlay Widget Class uninitialized, please fill out BP_AuraHUD"));
+	checkf(OverlayWidgetControllerClass, TEXT("Overlay Widget Controller Class uninitialized, please fill out BP_AuraHUD"));
+	
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	OverlayWidget = Cast<UAuraUserWidget>(Widget);
+
+	FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AttributeSet);
+	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
+
+	OverlayWidget->SetWidgetController(WidgetController);
+	
+	Widget->AddToViewport();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WcParams)
+{
+	PlayerController = WcParams.PlayerController;
+	PlayerState = WcParams.PlayerState;
+	AbilitySystemComponent = WcParams.AbilitySystemComponent;
+	AttributeSet = WcParams.AttributeSet;
+}
